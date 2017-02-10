f53a5ef046e278d68b0477b2a3330a00fe74194d
Jorge Guerra <poroporque@hotmail..com>
2017-02-09 20:52:36 -0500

    Solución reto 9

--- challenges/9-triangle-side/my_solution.rb
+++ challenges/9-triangle-side/my_solution.rb
@@ -2,4 +2,12 @@
 
 # Tu solucion abajo:
 
-
+def valid_triangle? (x, y, z)
+  if x <= 0 && y <= 0 && z <= 0
+    false
+  elsif (x + y) <= z || (x + z) <= y || (y + z) <= x
+    false
+  else
+    true
+  end
+end
