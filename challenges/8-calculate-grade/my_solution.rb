f753dc7a0503b51beb2407ae45449286847b4b97
Jorge Guerra <poroporque@hotmail..com>
2017-02-09 20:28:09 -0500

    Solución reto 8

--- challenges/8-calculate-grade/my_solution.rb
+++ challenges/8-calculate-grade/my_solution.rb
@@ -2,3 +2,16 @@
 
 # Tu solucion abajo:
 
+def get_grade (x)
+  if x.to_i >= 90
+    "A"
+  elsif x.to_i >= 80
+    "B"
+  elsif x.to_i >= 70
+    "C"
+  elsif x.to_i >= 60
+    "D"
+  elsif x.to_i < 60
+    "F"
+  end
+end
