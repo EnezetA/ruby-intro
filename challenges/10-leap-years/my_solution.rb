84c8b362b796d36bc857876567a3c3d9a2719e2c
Jorge Guerra <poroporque@hotmail..com>
2017-02-10 18:16:37 -0500

    Solución reto 10

--- challenges/10-leap-years/my_solution.rb
+++ challenges/10-leap-years/my_solution.rb
@@ -1,7 +1,17 @@
 # Leap Years
-
+# divisible por 400 y 4
 
 
 
 # Tu solucion abajo:
-
+def leap_year?(num)
+  if num % 4 == 0 && num % 100 != 0
+    true
+  elsif num % 400 == 0
+    true
+  elsif num % 4 == 0 && num % 100 == 0 && num % 400 != 0
+    false
+  elsif num % 4 != 0
+    false
+  end
+end
