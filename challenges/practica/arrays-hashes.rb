1ba9bf78de16f3a3d12ca3ba497d432f74bd3464
Jorge Guerra <poroporque@hotmail..com>
2017-02-15 19:54:02 -0500

    solcuiones

--- challenges/practica/arrays-hashes.rb
+++ challenges/practica/arrays-hashes.rb
@@ -1,27 +1,40 @@
 
 # Practica de Arrays
 
-zombie_apocalypse_supplies = ["hatchet", "rations", "water jug", "binoculars",
-                              "shotgun", "compass", "CB radio", "Tactical 10mm"]
+
+zombie_apocalypse_supplies = ["hatchet", "rations", "water jug", "binoculars", "shotgun", "compass", "CB radio", "Tactical 10mm"]
+
+
+result == ["binoculars", "CB radio", "compass", "hatchet", "rations", "shotgun", "water jug", "Tactical 10mm"]
 
 # 1. Itera sobre el array zombie_apocalypse_supplies, imprimiendo cada elemento dentro del array.
 # ----
+zombie_apocalypse_supplies.each do |x|
+  puts x
+end
 
 # 2. Para mantenernos organizados, organiza el array zombie_apocalypse_supplies de forma alfabetica.
 # ----
+zombie_apocalypse_supplies.sort!
 
- result == ["binoculars", "CB radio", "compass", "hatchet", "rations","shotgun", "Tactical 10mm", "water jug"]
+
 
 # 3. Crea un metodo que mire si un item (string) esta dentro del arreglo zombie_apocalypse_supplies. Por ejemplo esta `boots` dentro de la lista de suministros?.
 # ----
+def include_str(str, arr)
+  arr.include?(str)
+end
 
 # 4. No puedes cargar muchas cosas, solo hay espacio para 5. Remueve todos los items del arreglo zombie_apocalypse_supplies que tengas mas de dos palabras, solo pueden quedar los 5 items compuestos de una sola palabra.
 # ----
+zombie_apocalypse_supplies.delete_if {|supply| supply.include?(" ")}
+
 
 # 5. Encontraste otro sobreviviente! esto significa que pueden combinar sus suministros. Crea un nuevo arreglo de suministros combinados en base a tus zombie_apocalypse_supplies, y los sumnitros del otro sobreviviente. Debes deshacerte de todos los suministros que esten duplicados. No olvides revisar la documentacion de Array.
-other_survivor_supplies = [ "warm clothes", "rations", "compass", "camp stove",
-                            "solar battery", "flashlight"]
+other_survivor_supplies = [ "warm clothes", "rations", "compass", "camp stove","solar battery", "flashlight"]
 # ----
+suministros_combinados = zombie_apocalypse_supplies | other_survivor_supplies
+
 
 # Practica de Hash
 
@@ -37,12 +50,19 @@ extinct_animals = {
 
 # 1. Itera sobre el hash extinct_animals, imprimiendo cada vez el la pareja de key/value con un dash (-) entre ellos y un asterisco (*) entre cada elemento (animal extincto).
 # ----
+extinct_animals.each do |key, value|
+  puts "#{key} - #{value}"
+end
 
 # 2. Elimina todos los animales que se extinguieron despues del año 1999, del hash extinct_animals. No uses metodos especiales solo `delete` e iteraciones.
 # ----
+extinct_animals.delete_if {|key, value| value > 1999}
 
 # 3. Nuestros calculos estaban muy mal, resulta que todos esos animales se extinguieron 3 años antes que la fecha dada. Actualiza los valores dentro de extinct_animals para que reflejen el valor real de la fecha de extincion.
 # ----
+extinct_animals.each do |key, value|
+  extinct_animals[key] = value - 3
+end
 
 # 4. Oiste que los siguientes animales podian estar extinctos, pero no estas seguro. Uno por uno revisa si se encuentran incluidos dentro del hash extinct_animals:
 # "Andean Cat"
@@ -50,55 +70,13 @@ extinct_animals = {
 # "Tasmanian Tiger"
 # "Saiga Antelope"
 # ----
+extinct_animals.has_value?("Andean Cat")
+extinct_animals.has_value?("Dodo")
+extinct_animals.has_value?("Andean Tasmanian Tiger")
+extinct_animals.has_value?("Saiga Antelope")
 
 # 5. Acabamos de encontrar que el Passenger Pigeon, realmente no esta extincto!
 # Remuevelo del hash extinct_animals y devuelve su pareja de key/value como un arreglo de dos elementos. Puedes encontrar un metodo en la documentacion de la  clase Hash que te puede ayudar con esto.
 # ----
-
-
-# Practica, Estructuras nesteadas
-
-# Imprime en la consola solo el elemento target ningun otro
-
-# Target: "FORE"
-
-array = [[1,2], ["inner", ["eagle", "par", ["FORE", "hook"]]]]
-
-# Intentos:
-# ============================================================
-
-
-
-# ============================================================
-
-# Target: "congrats!"
-
-hash = {outer: {inner: {"almost" => {3 => "congrats!"}}}}
-
-# Intentos:
-# ============================================================
-
-
-
-# ============================================================
-
-# Target: "finished"
-
-nested_data = {array: ["array", {hash: "finished"}]}
-
-# Intentos:
-# ============================================================
-
-
-
-# ============================================================
-
-#Usando un metodo del modulo Enumerable, modifica el array number_array de forma que cada numero se le sume 5. Lo vas a hacer de forma destructiva o no ?
-
-number_array = [5, [10, 15], [20,25,30], 35]
-
-
-
-# Crea un metodo que recina un array como argumento, en este caso el array startup_names, y devuelva un array igual pero en donde a cada nombre se le a añadido 'ly' al final.
-
-startup_names = ["bit", ["find", "fast", ["optimize", "scope"]]]
+aint_extinct = extinct_animals.select{|k, v| k == "Passenger Pigeon"}
+aint_extinct.to_a
