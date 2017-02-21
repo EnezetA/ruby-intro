4048eab195bd04ed52eb9fdd4c0a497cfe5fe763
Jorge Guerra <poroporque@hotmail..com>
2017-02-21 18:11:01 -0500

    subiendo todo

--- challenges/22-regex/my_solution.rb
+++ challenges/22-regex/my_solution.rb
@@ -4,6 +4,11 @@
 # Determina si un string contiene un SSN o no.
 def has_ssn?(string)
   # tu codigo va aca
+  if string.match(/\d{3}-\d{2}-\d{4}/)
+    true
+  else
+    false
+  end
 end
 
 puts "has_ssn? returns true if it has what looks like a SSN"
@@ -16,6 +21,7 @@ puts has_ssn?("please confirm your identity: XXX-XX-1422") == false
 # Busca el numero SSN que se encuntra dentro de un string, si hay uno dentro de ella. De lo contrario devuelve nil.
 def grab_ssn(string)
   # tu codigo va aca
+  return nil if !string.match(/\d{3}-\d{2}-\d{4}/)
 end
 
 
@@ -29,9 +35,12 @@ puts grab_ssn("please confirm your identity: XXX-XX-1422") == nil
 # Devuelve todos los SSN dentro de un string. Si no hay ninguno devuelve un arreglo Vacio.
 def grab_all_ssns(string)
   # tu codigo va aca
+  return [] if !string.scan(/\d{3}-\d{2}-\d{4}/)
 end
 
+
 puts "grab_all_ssns returns all SSNs if the string has any SSNs"
+p grab_all_ssns("234-60-1422, 350-80-0744, 013-60-8762")
 puts grab_all_ssns("234-60-1422, 350-80-0744, 013-60-8762") == ["234-60-1422", "350-80-0744", "013-60-8762"]
 
 puts "grab_all_ssns returns an empty Array if it doesn't have any SSNs"
@@ -41,9 +50,16 @@ puts grab_all_ssns("please confirm your identity: XXX-XX-1422") == []
 # Ofusca todos los SSN dentro de un string, por ejmplo 123-55-4430 se convierte en  XXX-XX-4430. Deuvuele el string con los SSB ofuscados.
 def hide_all_ssns(string)
   # tu codigo va aca
+  match = string.scan(/\d{3}-\d{2}-\d{4}/)
+  match.map do |elem|
+    elem.gsub(/\d{3}-\d{2}/,"XXX-XX")
+  end
+
 end
 
+
 puts "hide_all_ssns obfuscates any SSNs in the string"
+p hide_all_ssns "234-60-1422, 350-80-0744, 013-60-8762"
 puts hide_all_ssns("234-60-1422, 350-80-0744, 013-60-8762") == "XXX-XX-1422, XXX-XX-0744, XXX-XX-8762"
 
 puts "hide_all_ssns does not alter a string without SSNs in it"
